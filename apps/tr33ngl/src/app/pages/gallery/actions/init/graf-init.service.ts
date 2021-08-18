be3205165b7337330e57c1bac454f42cc26f318e
risavkarna <risavkarna@gmail.com>
2021-08-18 12:23:01 +0545

    (minor) add h-graph append/prepend flow

--- apps/tr33ngl/src/app/pages/gallery/actions/init/graf-init.service.ts
+++ apps/tr33ngl/src/app/pages/gallery/actions/init/graf-init.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from "@angular/core";
 import { Edge, Edges, Graph, Node, Nodes } from "../../models/h-graph.model";
-import { HGraph } from "@cosys/func";
+import { HGraph, Listoid } from "@cosys/func";
 import { SimpleGraphInit } from "./simple-graph-init";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GrafInitService {
-  private nodes!: Nodes;
-  private edges!: Edges;
 
-  init( graph: { nodes: Nodes, edges: Edges} ) {
+  private readonly nodes: Nodes = new Listoid<Node[]>([]);
+  private readonly edges: Edges = new Listoid<Edge[]>([]);
+
+  prepend( graph: { nodes: Nodes, edges: Edges} ) {
     const { nodes, edges } = graph;
-    this.nodes = nodes;
-    this.edges = edges;
+    this.nodes.value = [...this.nodes.value, ...nodes.value];
+    this.edges.value = [...this.edges.value, ...edges.value];
+  }
+
+  append( graph: { nodes: Nodes, edges: Edges} ) {
+    const { nodes, edges } = graph;
+    this.nodes.value = [...nodes.value, ...this.nodes.value];
+    this.edges.value = [...edges.value, ...this.edges.value];
   }
 
   get graph(): Graph {
