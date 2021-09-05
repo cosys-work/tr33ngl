f85e6ed97ee13d32536ef6578ee6893cf09a2de7
risavkarna <risavkarna@gmail.com>
2021-09-05 12:46:34 +0545

    (update) 0.2.9 fix export
    Former-commit-id: 8125b1c98ce09e3bac7acded7db0a36ebba91ab9

--- apps/api/src/app/acktion/zentrale.module.ts
+++ apps/api/src/app/acktion/zentrale.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ZentraleService } from './zentrale.service';
 
 @Module({
-  providers: [ZentraleService]
+  providers: [ZentraleService],
+  exports: [ZentraleService]
 })
 export class ZentraleModule {}
