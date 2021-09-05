b8c3f7ee3128edf76f20e27712fc6047558e9bb3
risavkarna <risavkarna@gmail.com>
2021-09-05 10:26:18 +0545

    (update) 0.2.9 add api scaffolding
    Former-commit-id: 2f82cdeeffb8112734d2342b4256bca1282ac159

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ZentraleModule} from "./acktion/zentrale.module";
 
 @Module({
-  imports: [],
+  imports: [ZentraleModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,34 @@
 export interface Message {
   message: string;
 }
+
+
+export interface Session {}
+
+export interface OpenConf {}
+
+export interface LoginConf {}
+
+export interface GrepConf {}
+
+export interface Conf {}
+
+export interface UIComp {}
+
+export interface Login {}
+
+export interface Grep {}
+
+export interface SaveOrTrac {}
+
+export interface ScanOrAuto {}
+
+export interface Gra<T> {}
+
+export interface Response<T> {
+
+}
+
+export interface Request<T> {
+
+}
